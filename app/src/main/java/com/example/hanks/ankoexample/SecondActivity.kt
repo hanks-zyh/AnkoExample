9ca6e3922a33c1d44aa6ce137a7b8c527d677978
hanks-zyh <zhangyuhan2014@gmail.com>
2015-10-22 09:14:36 +0800

    init project

---
+++ app/src/main/java/com/example/hanks/ankoexample/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.example.hanks.ankoexample
+
+import android.app.Activity
+import android.os.Bundle
+import org.jetbrains.anko.*
+
+/**
+ * Created by hanks on 15-10-21.
+ */
+class SecondActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
+
+    }
+}
