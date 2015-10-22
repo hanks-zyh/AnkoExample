f1d7749d23d49bd450fb367b2e5e280f86be3fac
hanks-zyh <zhangyuhan2014@gmail.com>
2015-10-22 09:16:15 +0800

    setOnClickListener ; startActivity

--- app/src/main/java/com/example/hanks/ankoexample/MainActivity.kt
+++ app/src/main/java/com/example/hanks/ankoexample/MainActivity.kt
@@ -18,29 +18,24 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
+        //setOnClickListener
         fab.setOnClickListener {
             Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //startActivity
             startActivity<SecondActivity>()
             return true
         }
-
         return super.onOptionsItemSelected(item)
     }
 }
